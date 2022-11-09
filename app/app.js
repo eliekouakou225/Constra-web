85c72fc6567ef834dc8180eeb3c4b429c0968fa6
Manu <emmanuelelienhokouakou9@gmail.com>
2022-11-09 19:52:30 +0000

    Finalisation de la section RecentProjet

--- app/app.js
+++ app/app.js
@@ -1,6 +1,26 @@
-const searchButton = document.getElementById('search-button');
-const searchInput = document.getElementById('search-input');
-searchButton.addEventListener('click', () => {
-  const inputValue = searchInput.value;
-  alert(inputValue);
+// const searchButton = document.getElementById('search-button');
+// const searchInput = document.getElementById('search-input');
+// searchButton.addEventListener('click', () => {
+//   const inputValue = searchInput.value;
+//   alert(inputValue);
+// });
+
+// -------------Skill Section--------------
+
+let valueDisplays = document.querySelectorAll(".skill__num");
+let interval = 2000;
+
+
+valueDisplays.forEach((valueDisplay) =>{
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function(){
+    startValue += 1;
+
+    valueDisplay.textContent = startValue;
+    if(startValue == endValue ){
+      clearInterval(counter);
+    }
+  }, duration);
 });
